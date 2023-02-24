2f6d85941e825555af5b981884e3794b7ffb538d
leaskeg <leaskegaming@gmail.com>
2023-02-24 13:41:14 +0100

    Preview Added To Image. With of Javascript (site.js file)

--- SurfBoardApp/wwwroot/js/site.js
+++ SurfBoardApp/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Write your JavaScript code.
+function readURL(input) { //Defines a function named readURL that takes an input element as its argument.
+    if (input.files && input.files[0]) { // Checks if the input element has a files property and if the first file in the array exists.
+        var reader = new FileReader(); //Creates a new FileReader object.
 
-// Write your JavaScript code.
+        reader.onload = function (e) { //Defines an event handler function to be executed when the load event is fired by the FileReader.
+            $('#preview').attr('src', e.target.result).show(); // Sets the src attribute of the image element with ID preview to the data URL of the loaded file, and shows the element.
+        };
+
+        reader.readAsDataURL(input.files[0]); //Starts reading the first file in the input element's files array as a data URL.
+    }
+}
+
+$("#image").change(function () { //Defines an event handler function to be executed when the value of the input element with ID image changes.
+    readURL(this); //Calls the readURL function with this as its argument, which refers to the input element with ID image. This triggers the loading and previewing of the selected image file.
+});
+
