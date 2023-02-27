007371b9ad7d87bba3afbdd274aa738554b2cfa3
RTNordkvist <rikkenordkvist@hotmail.com>
2023-02-27 10:02:45 +0100

    Validation for all properties on board added

--- SurfBoardApp/Models/Board.cs
+++ SurfBoardApp/Models/Board.cs
@@ -12,16 +12,27 @@ namespace SurfBoardApp.Models
         [Required]
         [MinLength(1, ErrorMessage ="The board name must be at least 1 character long")]
         public string? Name { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "The length must be a positive value")]
         public double? Length { get; set; }
+        
+        [Range(0.0, double.MaxValue, ErrorMessage = "The width must be a positive value")]
         public double? Width { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "The thickness must be a positive value")]
         public double? Thickness { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "The volume must be a positive value")]
         public double? Volume { get; set; }
+        
         public string? Type { get; set; }
 
         [Required]
         [Range(0.0, (double)decimal.MaxValue, ErrorMessage="The price must be a positive value")]
         public decimal? Price { get; set; }
+        
         public string? Equipment { get; set; }
+        
         public List<Image>? Images { get; set; }
     }
 }
