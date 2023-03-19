697aac5da043ed53f1e3ad2c72e3f19ce3aa2c11
leaskeg <leaskegaming@gmail.com>
2023-03-19 02:07:11 +0100

    Comments added

--- SurfBoardApp/Controllers/ExternalAuthenticationController.cs
+++ SurfBoardApp/Controllers/ExternalAuthenticationController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿// This code file defines the ExternalAuthenticationController class in the SurfBoardApp.Controllers namespace
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SurfBoardApp.Data.Models;
+using System;
 using System.Security.Claims;
 
 namespace SurfBoardApp.Controllers
@@ -10,17 +12,21 @@ namespace SurfBoardApp.Controllers
     public class ExternalAuthenticationController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+
+        // Action method to display the Index view
         public IActionResult Index()
         {
             return View();
         }
 
+        // Action method to initiate Google authentication
         public IActionResult Google()
         {
             var properties = new AuthenticationProperties { RedirectUri = "https://localhost/" };
             return Challenge(properties, "Google");
         }
 
+        // Action method to handle the Google authentication callback
         public async Task<IActionResult> GoogleCallback()
         {
             var result = await HttpContext.AuthenticateAsync("ExternalCookie");
@@ -44,8 +50,8 @@ namespace SurfBoardApp.Controllers
 
             var identity = new ClaimsIdentity(new[]
             {
-        new Claim(ClaimTypes.Name, user.Email),
-        new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Name, user.Email),
+            new Claim(ClaimTypes.Email, user.Email),
         }, CookieAuthenticationDefaults.AuthenticationScheme);
 
             var principal = new ClaimsPrincipal(identity);
@@ -54,7 +60,5 @@ namespace SurfBoardApp.Controllers
 
             return RedirectToAction("Index", "Home");
         }
-
-
     }
 }
--- SurfBoardApp/PaginatedList.cs
+++ SurfBoardApp/PaginatedList.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿// This class represents a paginated list of items of type T
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,34 +7,50 @@ using System.Threading.Tasks;
 
 namespace SurfBoardApp
 {
+    //Used for the surf board pages
     public class PaginatedList<T> : List<T>
     {
+        // The current page index
         public int PageIndex { get; private set; }
+
+        // The total number of pages
         public int TotalPages { get; private set; }
 
+        // Constructor to create a new instance of PaginatedList
         public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
         {
+            // Set the current page index
             PageIndex = pageNumber;
+
+            // Calculate the total number of pages
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
+            // Add the items to the list
             AddRange(items);
         }
 
+        // Indicates if there is a previous page
         public bool HasPreviousPage
         {
             get { return PageIndex > 1; }
         }
 
+        // Indicates if there is a next page
         public bool HasNextPage
         {
-
             get { return PageIndex < TotalPages; }
         }
 
+        // Creates a new instance of PaginatedList asynchronously
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
+            // Count the total number of items in the source
             var count = await source.CountAsync();
+
+            // Get the items for the current page
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            // Return a new instance of PaginatedList
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
