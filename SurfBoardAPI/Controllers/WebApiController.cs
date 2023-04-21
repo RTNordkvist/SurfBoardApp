64f96e313b40edf65dee50d12c45dfead4976d9a
leaskeg <leaskegaming@gmail.com>
2023-04-21 12:53:55 +0200

    added comments and made the API more in line with RESTful

--- SurfBoardAPI/Controllers/WebApiController.cs
+++ SurfBoardAPI/Controllers/WebApiController.cs
@@ -2,106 +2,125 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BoardModel = SurfBoardApp.Data.Models.Board;
 using SurfBoardApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WebAPISurf.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class WebApiController : ControllerBase
+    // forklaring på hvorfor vi bruger [Route("api/boards")] istedet for [Route("api/[controller] : 
+    //Brug [Route("api/boards")]i stedet for [Route("api/[controller]")] anses for at være mere RESTful,
+    //fordi det overholder de almindelige konventioner for en RESTful API.
+    //RESTful API'er anbefales det at bruge små bogstaver og flertal ressourcenavne i URL'erne.
+    //Dette gør API'en mere konsistent og lettere for kunder at forstå og bruge.
+    [Route("api/boards")] 
+    public class BoardController : ControllerBase
     {
-        private readonly SurfBoardAppContext _dbcontext;
+        private readonly SurfBoardAppContext _context;
 
-        public WebApiController(SurfBoardAppContext context)
+        public BoardController(SurfBoardAppContext context)
         {
-            _dbcontext = context;
+            _context = context;
         }
 
+        // Get a list of all boards
         [HttpGet]
-        [Route("Board")]
-        public async Task<IActionResult> GetBoard()
+        public async Task<IActionResult> GetBoards()
         {
             try
             {
-                List<BoardModel> listBoards = _dbcontext.Board.Select(b => new BoardModel { Id = b.Id, Name = b.Name }).ToList();
-                if (listBoards != null)
+                var boards = await _context.Board
+                    .Select(b => new BoardModel { Id = b.Id, Name = b.Name })
+                    .ToListAsync();
+
+                if (boards != null && boards.Count > 0)
                 {
-                    return Ok(listBoards);
+                    return Ok(boards);
                 }
-                return Ok("There are no boards in the database");
+                else
+                {
+                    return NotFound("No boards found in the database");
+                }
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
 
+        // Create a new board
         [HttpPost]
-        [Route("Board")]
         public async Task<IActionResult> CreateBoard([FromBody] SurfBoardApp.Data.Models.Board board)
         {
             try
             {
-                if (board != null)
+                if (board == null)
                 {
-                    _dbcontext.Board.Add(board);
-                    _dbcontext.SaveChanges();
-                    return Ok("Board created successfully");
+                    return BadRequest("Board object is null");
                 }
-                return BadRequest("Board object is null");
+
+                _context.Board.Add(board);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetBoard), new { id = board.Id }, board);
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
 
-        [HttpDelete("Board {id}")]
+        // Delete a board by ID
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBoard(int id)
         {
-            var board = await _dbcontext.Board.FindAsync(id);
+            var board = await _context.Board.FindAsync(id);
             if (board == null)
             {
-                return NotFound();
+                return NotFound($"Board with ID {id} not found");
             }
 
-            _dbcontext.Board.Remove(board);
-            await _dbcontext.SaveChangesAsync();
+            _context.Board.Remove(board);
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-        [HttpGet("Board {id}")]
+        // Get a specific board by ID
+        [HttpGet("{id}")]
         public async Task<ActionResult<SurfBoardApp.Data.Models.Board>> GetBoard(int id)
         {
-            var board = await _dbcontext.Board.FindAsync(id);
+            var board = await _context.Board.FindAsync(id);
 
             if (board == null)
             {
-                return NotFound();
+                return NotFound($"Board with ID {id} not found");
             }
 
-            return board;
+            return Ok(board);
         }
 
-        [HttpPut("Board {id}")]
+        // Update a board by ID
+        [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBoard(int id, SurfBoardApp.Data.Models.Board board)
         {
             if (id != board.Id)
             {
-                return BadRequest();
+                return BadRequest("Board ID mismatch");
             }
-
-            _dbcontext.Entry(board).State = EntityState.Modified;
 
             try
             {
-                await _dbcontext.SaveChangesAsync();
+                _context.Entry(board).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!BoardExists(id))
                 {
-                    return NotFound();
+                    return NotFound($"Board with ID {id} not found");
                 }
                 else
                 {
@@ -112,9 +131,10 @@ namespace WebAPISurf.Controllers
             return NoContent();
         }
 
-        private bool BoardExists(long id)
+        // Check if a board with a specific ID exists
+        private bool BoardExists(int id) // Changed parameter type to int for consistency
         {
-            return _dbcontext.Board.Any(e => e.Id == id);
+            return _context.Board.Any(e => e.Id == id);
         }
     }
 }
